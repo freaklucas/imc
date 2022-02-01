7cecf58b487d3adbd9671b5037b5d3b16413ac6d
freaklucas <lucas.oliveira.freak@gmail.com>
2022-02-01 00:08:32 -0300

    funcionalidades ok

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ export default function App() {
   const [peso, setPeso] = useState("");
   const [altura, setAltura] = useState("");
 
+  const [msg, setMsg] = useState("");
+
+  function calcImc() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if (imc < 18.6) {
+      setMsg("Você está Abaixo do peso " + imc.toFixed(2));
+    } else if (imc >= 18.6 && imc < 24.9) {
+      setMsg("Você está no peso ideal " + imc.toFixed(2));
+    } else if (imc >= 24.9 && imc < 34.9) {
+      setMsg("Você está Acima do peso ideal " + imc.toFixed(2));
+    } else if (imc > 34.9) {
+      setMsg("Cuidado você está com obesidade! " + imc.toFixed(2));
+    }
+  }
+
   return (
     <div className="app">
       <h1>Calculadora IMC</h1>
@@ -20,15 +37,15 @@ export default function App() {
 
         <input
           type="text"
-          placeholder="Peso em (kg) Ex: 123"
+          placeholder="Altura em (cm) Ex: 193"
           value={altura}
           onChange={(e) => setAltura(e.target.value)}
         />
 
-        <button>Calcular</button>
+        <button onClick={calcImc}>Calcular</button>
       </div>
 
-      <h2>Seu imc foi 25, você está abaixo do Peso!</h2>
+      <h2>{msg}</h2>
     </div>
   );
 }
