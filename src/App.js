45771039ae0409a6726fb0c020c111a26ada8e4a
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-31 23:49:54 -0300

    config setup + useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import "./app.css";
+
 export default function App() {
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+
   return (
-    <div>
+    <div className="app">
       <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu imc</span>
+
+      <div className="area-input">
+        <input type="text" placeholder="Peso em (kg) Ex: 123" value={peso} />
+        <input type="text" placeholder="Peso em (kg) Ex: 123" value={altura} />
+
+        <button>Calcular</button>
+      </div>
+
+      <h2>Seu imc foi 25, você está abaixo do Peso!</h2>
     </div>
   );
 }
